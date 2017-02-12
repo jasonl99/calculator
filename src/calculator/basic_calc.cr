4910312ec8a1f5ab0374ea2e68bca3e1dddefd29
Jason Landry <jasonl99@gmail.com>
2017-02-12 10:49:22 -0500

    wip

--- src/calculator/basic_calc.cr
+++ src/calculator/basic_calc.cr
@@ -1,6 +1,10 @@
 module Calculator
   class BasicCalc < Lattice::Connected::StaticBuffer
     def on_event (event, sender)
+      if event.direction == "In" && event.message_value("action") == "submit"
+        val = event.message_value("params,entry").as(String)
+        add_content val, dom_id("entries")
+      end
       puts "#{self.class} #{event.event_type} #{event.dom_item} #{event.direction} #{event.message}"
     end
   end
